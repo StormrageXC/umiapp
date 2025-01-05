f92aba8e969b0ab8700f87a4c0f5039835dd67a4
StormrageXC <35964016+StormrageXC@users.noreply.github.com>
2025-01-05 13:50:54 +0800

    Update app.ts2

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,6 @@
 export async function getInitialState(): Promise<{ name: string }> {
   return { name: '@umijs/max' };
 }
-
-
 export const layout = () => {
   return {
     logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
